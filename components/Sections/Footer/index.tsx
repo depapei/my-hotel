7b32f02b692a34a5ad364eda9535fa002d6d0e33
Iamriss <moriswirantara2003@gmail.com>
2026-02-13 01:06:02 +0700

    add footer

---
+++ components/Sections/Footer/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'lucide-react';
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-blue-500 flex z w-full justify-center">
+      <div className="flex p-5">
+          <h1 className="text-white font-semibold"> copyright 2026 &copy; Hotel Sinar Pelangi</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
