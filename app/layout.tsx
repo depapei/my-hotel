a7413305ac1e481a2071f6ce7cd626303c8fcf3e
RANGGA ANDIKA WIBISANA <ranggaa606@gmail.com>
2026-02-15 21:57:00 +0700

    change web title wording (browser tab title)

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Hotel Booking",
+  title: "Hotel Sinar Pelangi",
   description: "Mau booking hotel? Disini aja!",
 };
 
